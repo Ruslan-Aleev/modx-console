bc3d6cde173f005cfd0ea4e383fcfaceae4cad25
Ruslan Aleev <aleev.insmile@gmail.com>
2024-09-26 21:02:38 +0400

    Added Console to title
    Added Console to the title. This makes it easier to find if you have a lot of open tabs.

--- core/components/console/controllers/index.class.php
+++ core/components/console/controllers/index.class.php
@@ -1,19 +1,23 @@
 <?php
-
 require_once __DIR__ . "/../console.class.php";
 
 class ConsoleIndexManagerController extends ConsoleManagerController{
-    
-    function process(array $scriptProperties = array()) {
 
-        #add js
+    function process(array $scriptProperties = array())
+    {
         $this->addJavascript($this->config['manager_url'].'js/widgets/console.panel.js');
         $this->addJavascript($this->config['manager_url'].'js/sections/home.js');
 
         $this->modx->invokeEvent('OnSnipFormPrerender');
     }
-    
-    function getTemplateFile() {
+
+    public function getPageTitle()
+    {
+        return $this->modx->lexicon('console');
+    }
+
+    function getTemplateFile()
+    {
         return $this->getTemplate('index.tpl');
     }
 }
